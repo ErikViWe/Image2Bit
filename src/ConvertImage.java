2e342774c2bb206f320c7b6999656a2424722978
ErikViWe <erik.weinstock@googlemail.com>
2017-06-25 10:45:17 +0200

    Removed unused imports

--- src/ConvertImage.java
+++ src/ConvertImage.java
@@ -1,7 +1,5 @@
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.Writer;
 
 /**
  * This class contains all methods dealing with the imported image.
